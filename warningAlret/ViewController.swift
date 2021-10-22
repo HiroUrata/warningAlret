0ff94500700d7f987059d3ee9f95f41730513c55
HiroUrata <1712026@i.softbank.jp>
2021-10-22 22:22:03 +0900

    judgeText　judge　処理を追加前

--- warningAlret/ViewController.swift
+++ warningAlret/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        textField.addTarget(self, action: #selector(judgeText), for: .valueChanged)
     }
 
-
+    @objc func judgeText(sender:UITextField){
+        
+        if sender.text?.count == 4{
+            
+        }
+        
+    }
+    
+    func judge(judgeTarget:String){
+        
+        
+    }
 }
 

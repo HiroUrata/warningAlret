54646d44bacbfdd5a727d23855f2e3f53b8a7797
HiroUrata <1712026@i.softbank.jp>
2021-10-22 22:13:41 +0900

    Initial Commit

---
+++ warningAlret/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  warningAlret
+//
+//  Created by UrataHiroki on 2021/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

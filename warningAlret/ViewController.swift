2be29ae565b566e3ddcd12deac1781ce38f39b45
HiroUrata <1712026@i.softbank.jp>
2021-10-22 23:54:24 +0900

    連番判定機能　追加

--- warningAlret/ViewController.swift
+++ warningAlret/ViewController.swift
@@ -8,27 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var textField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        textField.addTarget(self, action: #selector(judgeText), for: .valueChanged)
+        
+        textField.addTarget(self, action: #selector(judgeText), for: .editingChanged)
     }
-
+    
     @objc func judgeText(sender:UITextField){
         
         if sender.text?.count == 4{
             
+            judge(judgeTarget: sender.text!)
         }
         
     }
     
     func judge(judgeTarget:String){
         
+        if Int(judgeTarget.prefix(2).suffix(1)) == Int(judgeTarget.prefix(1))! + 1 && Int(judgeTarget.prefix(3).suffix(1)) == Int(judgeTarget.prefix(2).suffix(1))! + 1 &&
+            Int(judgeTarget.suffix(1)) == Int(judgeTarget.prefix(3).suffix(1))! + 1{
+            
+            alert()
+            
+        }else if Int(judgeTarget.prefix(2).suffix(1)) == Int(judgeTarget.prefix(1))! - 1 && Int(judgeTarget.prefix(3).suffix(1)) == Int(judgeTarget.prefix(2).suffix(1))! - 1 && Int(judgeTarget.suffix(1)) == Int(judgeTarget.prefix(3).suffix(1))! - 1{
+            
+            alert()
+            
+        }else if judgeTarget.prefix(1) == judgeTarget.prefix(2).suffix(1) && judgeTarget.prefix(1) == judgeTarget.prefix(3).suffix(1) && judgeTarget.prefix(1) == judgeTarget.suffix(1){
+            
+            alert()
+        }
         
+    }
+    
+    func alert(){
+        
+        let alert = UIAlertController(title: "連番です", message: "連番だけど大丈夫かな？", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "キャンセル", style: .cancel, handler: nil))
+        
+        self.present(alert, animated:true, completion: nil)
     }
 }
 
